4e2b433f6f7137c0f2aca7f64a1d4ac32b0d5cd3
JuHeonParkk <ju10913@naver.com>
2026-02-10 10:52:02 +0900

    feat: sort food list latest and calorie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import items from "./mock.json";
 import FoodList from "./components/FoodList";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
   return (
     <div>
-      <FoodList items={items} />
+      <div>
+        <button onClick={() => setOrder("createdAt")}>최신순</button>
+        <button onClick={() => setOrder("calorie")}>칼로리순</button>
+      </div>
+      <FoodList items={sortedItems} />
     </div>
   );
 }
