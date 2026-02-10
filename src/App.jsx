15ebdd04fb8926dffa129e758f9f1a7fa21cac07
JuHeonParkk <ju10913@naver.com>
2026-02-10 10:55:42 +0900

    feat: add search feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,25 @@ import FoodList from "./components/FoodList";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [keyword, setKeyword] = useState("");
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const searchItems = sortedItems.filter(
+    (item) => item.title.includes(keyword) || item.content.includes(keyword),
+  );
 
   return (
     <div>
+      <input
+        type="text"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <div>
         <button onClick={() => setOrder("createdAt")}>최신순</button>
         <button onClick={() => setOrder("calorie")}>칼로리순</button>
       </div>
-      <FoodList items={sortedItems} />
+      <FoodList items={searchItems} />
     </div>
   );
 }
