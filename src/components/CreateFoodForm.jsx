2c29cd264f86e035b8e30d878ad4651c899bb4e5
JuHeonParkk <ju10913@naver.com>
2026-03-03 23:57:41 +0900

    refactor: input style change

--- src/components/CreateFoodForm.jsx
+++ src/components/CreateFoodForm.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import FileInput from "./FileInput";
 import Button from "./Button";
+import Input from "./Input";
 import styles from "./CreateFoodForm.module.css";
 
 export default function CreateFoodForm({
@@ -41,13 +42,13 @@ export default function CreateFoodForm({
         <FileInput name="imgUrl" initialPreview={initialValue.imgUrl} />
         <div className={styles.content}>
           <div className={styles.inputGroup}>
-            <input
+            <Input
               type="text"
               name="title"
               placeholder="이름을 입력하세요."
               ref={inputRef}
             />
-            <input type="number" name="calorie" placeholder="KCal" />
+            <Input type="number" name="calorie" placeholder="KCal" />
           </div>
           <textarea
             className={styles.textarea}
